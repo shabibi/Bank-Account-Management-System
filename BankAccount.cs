759b18e65fae014e92c3e98a66922a32149036cf
Afra Alshabibi <a.shabibi20@gmail.com>
2024-09-25 15:04:01 +0400

    Adding BankAccount private properties.

---
+++ BankAccount.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPPart1Task
+{
+    public class BankAccount
+    {
+        /// BankAccount private properties
+
+        public string AccountNumber { get;private set; }
+        public string AccountHolder { get;private set; }
+        public decimal Balance  { get;private set; }
+
+        
+
+    }
+}
